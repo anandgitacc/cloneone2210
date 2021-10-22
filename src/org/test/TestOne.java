78940df65aec4b8d2b4f42d00753b6f31ce3dc57
anandgitacc <anand.akmail@gmail.com>
2021-10-22 14:03:26 +0530

    angit

--- src/org/test/TestOne.java
+++ src/org/test/TestOne.java
@@ -12,6 +12,10 @@ private void empName() {
 		System.out.println("Employee mobile no is : 8667070832");
 	}
 	
+	private void empid() {
+		System.out.println("one");
+	}
+	
 	public static void main(String[] args) {
 		
 		TestOne ed = new TestOne();
