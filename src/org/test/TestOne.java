d54b80126cb922dcae369c5ec846aa965fe5e350
anandgitacc <anand.akmail@gmail.com>
2021-10-22 13:03:38 +0530

    gittestone

---
+++ src/org/test/TestOne.java
@@ -0,0 +1,24 @@
+package org.test;
+
+
+public class TestOne {
+	
+private void empName() {
+		
+		System.out.println("Employee Name is : Anand");
+	}
+	private void empMobNo() {
+		
+		System.out.println("Employee mobile no is : 8667070832");
+	}
+	
+	public static void main(String[] args) {
+		
+		TestOne ed = new TestOne();
+		
+		ed.empName();
+		ed.empMobNo();
+		
+	}
+
+}
